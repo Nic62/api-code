dc13aa70440eb63edc706142469b3624ea02f81a
Nicollas Matos <168081344+Nic62@users.noreply.github.com>
2026-02-24 12:57:47 -0300

    Add files via upload

--- site.py
+++ site.py
@@ -3,6 +3,8 @@ import streamlit as st
 #url
 #Local URL: http://localhost:8501
 # Network URL: http://192.168.1.1
+
+
 #dados
 df=pd.read_csv("database_log_todo.csv")
 #logo
@@ -10,9 +12,12 @@ col1, col2 = st.columns([6,1])
 
 with col2:
     st.image("logo.png", width=120)
-#site
-##titulo
+#paginas
+st.page_link("site.py", label="Home", icon="📊")
+st.page_link("pages/sitepesquisa.py", label="Pesquisa", icon="🔎")
+st.page_link("pages/dashonline.py", label="Dashboard", icon="📈")
 
+#titulo
 st.markdown(
     "<h1 style='text-align: center;'>API CODE</h1>",
     unsafe_allow_html=True
