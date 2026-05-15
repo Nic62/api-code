fdddcde8471b38b68f16cf85045771319c0bfd00
Nicollas Matos <168081344+Nic62@users.noreply.github.com>
2026-05-15 16:53:07 -0300

    Update sitepesquisa.py

--- pages/sitepesquisa.py
+++ pages/sitepesquisa.py
@@ -1,59 +1,103 @@
 import streamlit as st
 import pandas as pd
-#df
+from PIL import Image
+from pyzbar.pyzbar import decode
+
+# df
 @st.cache_data
 def carregar_dados():
-    return pd.read_csv("database_log_todo.csv",encoding="latin1",
-    sep=";",
-    on_bad_lines="skip")
+    return pd.read_csv(
+        "database_log_todo.csv",
+        encoding="latin1",
+        sep=";",
+        on_bad_lines="skip"
+    )
 
 df = carregar_dados()
 
 df = df[df["CLASSIF"] != "OBSOLETO"]
+
 df["CLASSIF"] = (
     df["CLASSIF"]
     .str.replace("_", " ", regex=False)
     .str.replace("+", "/", regex=False)
 )
-#logo
+
+# logo
 col1, col2 = st.columns([6,1])
 
 with col2:
     st.image("logo.png", width=120)
-    st.logo("logo.png")
-#paginas
+
+# paginas
 st.page_link("site.py", label="Home", icon="📊")
 st.page_link("pages/sitepesquisa.py", label="Pesquisa", icon="🔎")
-#titulo
+
+# titulo
 st.markdown(
     "<h1 style='text-align: center;'>PESQUISA</h1>",
     unsafe_allow_html=True
 )
-#pesquisa
+
+# pesquisa manual
 col1, col2, col3 = st.columns([1,2,1])
 
 with col2:
-    termo = st.text_input("Digite o código para pesquisa")
-#resposta
+    termo = st.text_input("Digite ou escaneie o código")
+
+# upload QR/barcode
+uploaded_file = st.file_uploader(
+    "Envie imagem do QR Code ou código de barras",
+    type=["png", "jpg", "jpeg"]
+)
+
+# leitura QR/barcode
+if uploaded_file is not None:
+
+    image = Image.open(uploaded_file)
+
+    decoded_objects = decode(image)
+
+    if decoded_objects:
+
+        termo = decoded_objects[0].data.decode("utf-8")
+
+        st.success(f"Código detectado: {termo}")
+
+    else:
+        st.error("Nenhum QR Code ou código de barras encontrado.")
+
+# resposta
 if termo:
-    resultados = df[df["CODIGO"].astype(str).str.lower().str.contains(termo.lower())]
+
+    resultados = df[
+        df["CODIGO"]
+        .astype(str)
+        .str.lower()
+        .str.contains(termo.lower())
+    ]
 
     st.markdown("<br>", unsafe_allow_html=True)
 
     col1, col2, col3 = st.columns([1,2,1])
 
     with col2:
+
         if not resultados.empty:
+
             for _, item in resultados.iterrows():
+
                 with st.container(border=True):
+
                     st.subheader(item["CODIGO"])
                     st.subheader(item["MODELO"])
                     st.subheader(item["DESCRICAO"])
                     st.subheader(item["ESTACAO"])
+
                     st.write(item["CONSUMO"])
                     st.write(item["SUB_GRUPO_1"])
                     st.write(item["SUB_GRUPO_2"])
                     st.write(item["CLASSIF"])
-        else:
 
+        else:
             st.warning("Nenhum resultado encontrado.")
