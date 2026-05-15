bc13c8bc436a6eaff9a095d09373a6133c40996d
Nicollas Matos <168081344+Nic62@users.noreply.github.com>
2026-05-15 16:28:13 -0300

    Update sitepesquisa.py

--- pages/sitepesquisa.py
+++ pages/sitepesquisa.py
@@ -3,7 +3,7 @@ import pandas as pd
 #df
 @st.cache_data
 def carregar_dados():
-    return pd.read_csv("database_log_todo.csv",encoding="latin1", sep=",")
+    return pd.read_csv("database_log_todo.csv")
 
 df = carregar_dados()
 
