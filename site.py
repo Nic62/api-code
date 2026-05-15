bc78e269c3d9c303d4208ccd7b3d9f0096b8362e
Nicollas Matos <168081344+Nic62@users.noreply.github.com>
2026-05-15 16:02:56 -0300

    Update site.py

--- site.py
+++ site.py
@@ -1,27 +1,16 @@
 import pandas as pd
 import streamlit as st
-#url
-#Local URL: http://localhost:8501
-# Network URL: http://192.168.1.1
 
-
-#dados
-df=pd.read_csv("database_log_todo.csv")
-#logo
+df = pd.read_csv("database_log_todo.csv")
 col1, col2 = st.columns([6,1])
 
 with col2:
     st.image("logo.png", width=120)
-st.logo("logo.png")
-#paginas
 st.page_link("site.py", label="Home", icon="📊")
 st.page_link("pages/sitepesquisa.py", label="Pesquisa", icon="🔎")
 
-
-#titulo
 st.markdown(
     "<h1 style='text-align: center;'>API CODE</h1>",
     unsafe_allow_html=True
 )
-
 st.dataframe(df)
